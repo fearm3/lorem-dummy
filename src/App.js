6b05512d66c44ebf2c944c3c58db66c2e41d4b19
fearm3 <ibraaa1628@gmail.com>
2021-12-22 14:09:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Hello world");
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input type="number" name="amount" id="amount" />
+      </form>
+    </section>
+  );
 }
 
 export default App;
